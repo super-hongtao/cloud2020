b21ad8dd71fc64649de33f02e9c856f65b949e85
super-hongtao <super_hongtao@163.com>
2020-07-15 00:33:59 +0800

    通用包对象抽取

---
+++ cloud-api-commons/src/main/java/com/ft/springcloud/entity/Payment.java
@@ -0,0 +1,21 @@
+package com.bz.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @program: cloud2020
+ * @description
+ * @author: HongTao
+ * @create: 2020-04-15 22:32
+ **/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment implements Serializable {
+    private Long id;
+    private String serial;
+}
