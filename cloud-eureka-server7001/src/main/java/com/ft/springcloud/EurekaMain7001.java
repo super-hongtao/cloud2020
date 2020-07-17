47954f94beca79a75faa0e332f6886b88c0d3a7d
super-hongtao <super_hongtao@163.com>
2020-07-17 22:54:18 +0800

    配置Eureka7001

---
+++ cloud-eureka-server7001/src/main/java/com/ft/springcloud/EurekaMain7001.java
@@ -0,0 +1,19 @@
+package com.ft.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author : HongTao
+ * @version : v1.0
+ * @description TODO
+ * @date : 2020/7/17 22:49
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class,args);
+    }
+}
