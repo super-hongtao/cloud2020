9f49e139b15ecbb7b4d18dd95b03c0b50c284903
super-hongtao <super_hongtao@163.com>
2020-07-18 00:29:34 +0800

    搭建eureka集群后将订单支付两个微服务注册进Eureka集群

---
+++ cloud-eureka-server7003/src/main/java/com/ft/springcloud/EurekaMain7003.java
@@ -0,0 +1,19 @@
+package com.ft.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author : HongTao
+ * @version : v1.0
+ * @description TODO
+ * @date : 2020/7/17 23:48
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7003 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7003.class,args);
+    }
+}
