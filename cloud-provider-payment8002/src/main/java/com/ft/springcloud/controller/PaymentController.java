88c499bd089d00a32f3487bc9ca816d88d4af2d1
super-hongtao <super_hongtao@163.com>
2020-07-21 23:32:08 +0800

    服务发现Discovery

--- cloud-provider-payment8002/src/main/java/com/ft/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/ft/springcloud/controller/PaymentController.java
@@ -7,9 +7,12 @@ import com.ft.springcloud.service.IPaymentService;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 
 /**
@@ -25,6 +28,8 @@ public class PaymentController {
     private IPaymentService ipaymentService;
     @Value("${server.port}")
     private String serverPort;
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     //只传给前端CommonResult，不需要前端了解其他的组件
     @PostMapping(value = "/payment/create")
@@ -46,6 +51,20 @@ public class PaymentController {
         }else{
             return new CommonResult(444,"没有对应记录,查询ID："+id,null);
         }
+    }
+
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();
+        for(String element : services){
+            log.info("****element: "+element);
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances){
+            log.info(instance.getServiceId()+"/t"+instance.getHost()+"/t"+instance.getPort()+"/t"+instance.getUri());
+        }
+        return this.discoveryClient;
     }
 
 }
