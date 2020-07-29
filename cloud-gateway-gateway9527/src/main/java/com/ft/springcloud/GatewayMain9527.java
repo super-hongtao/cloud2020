a0ff4bc00681683dddbf7a8678292b80664cead6
super-hongtao <super_hongtao@163.com>
2020-07-29 23:33:56 +0800

    搭建Gateway网关，通过application文件进行Gateway配置

---
+++ cloud-gateway-gateway9527/src/main/java/com/ft/springcloud/GatewayMain9527.java
@@ -0,0 +1,19 @@
+package com.ft.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author : HongTao
+ * @version : v1.0
+ * @description TODO
+ * @date : 2020/7/29 22:58
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class,args);
+    }
+}
---
+++ cloud-gateway-gateway9527/src/main/java/com/ft/springcloud/config/GateWayConfig.java
@@ -0,0 +1,14 @@
+//package com.ft.springcloud.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * @author : HongTao
+// * @version : v1.0
+// * @description TODO
+// * @date : 2020/7/29 23:16
+// */
+//@Configuration
+//public class GateWayConfig {
+//
+//}
