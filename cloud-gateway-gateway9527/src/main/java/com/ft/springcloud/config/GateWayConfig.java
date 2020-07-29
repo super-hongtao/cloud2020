51febc1fdc421c4df776f9b0305c08c2b9d6d628
super-hongtao <super_hongtao@163.com>
2020-07-30 00:04:08 +0800

    通过创建config配置文件进行编码对Gateway设置

--- cloud-gateway-gateway9527/src/main/java/com/ft/springcloud/config/GateWayConfig.java
+++ cloud-gateway-gateway9527/src/main/java/com/ft/springcloud/config/GateWayConfig.java
@@ -1,14 +1,44 @@
-//package com.ft.springcloud.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//
-///**
-// * @author : HongTao
-// * @version : v1.0
-// * @description TODO
-// * @date : 2020/7/29 23:16
-// */
-//@Configuration
-//public class GateWayConfig {
-//
-//}
+package com.ft.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author : HongTao
+ * @version : v1.0
+ * @description TODO
+ * @date : 2020/7/29 23:16
+ */
+@Configuration
+public class GateWayConfig {
+    /**
+     * 配置一个id为route-name的路由规则，
+     * 当访问地址http://localhost:9527/guonei时会自动转发到地址：http://news.baidu.com/guonei
+     * @param routeLocatorBuilder
+     * @return
+     */
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder){
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+
+        routes.route("path_route",r ->r.path("/guonei").uri("http://news.baidu.com/guonei")).build();
+        return routes.build();
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator2(RouteLocatorBuilder routeLocatorBuilder){
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+
+        routes.route("path_route",r ->r.path("/game").uri("http://news.baidu.com/game")).build();
+        return routes.build();
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator3(RouteLocatorBuilder routeLocatorBuilder){
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_route",r ->r.path("/internet").uri("http://news.baidu.com/internet")).build();
+        return routes.build();
+    }
+}
