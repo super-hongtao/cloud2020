3af2de0d9174fa6b1852a87317824ac976df8652
super-hongtao <super_hongtao@163.com>
2020-08-01 22:54:01 +0800

    Config配置总控中心3344搭建

---
+++ cloud-config-center-3344/src/main/java/com/ft/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.ft.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author : HongTao
+ * @version : v1.0
+ * @description TODO
+ * @date : 2020/8/1 0:23
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
